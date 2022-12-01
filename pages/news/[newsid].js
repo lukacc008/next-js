a2643f6e1e05dca040e955c282b82bd83fe1db54
unknown <lukacc008@gmail.com>
2022-12-01 15:34:25 +0100

    Extracted dynamic parameter values from url with useRouter

--- pages/news/[newsid].js
+++ pages/news/[newsid].js
@@ -1,6 +1,12 @@
+import { useRouter } from "next/router"
+
 // moj-domain.com/news/something-important
 
 function DetailPage() {
+  const router = useRouter();
+
+  console.log (router.query.newsid);
+
     return <h1>The Detail Page</h1>
   }
   
