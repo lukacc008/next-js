9ae05ac10a89522bc734d1230311afa7c39fcd76
unknown <lukacc008@gmail.com>
2022-12-01 16:11:52 +0100

    Linking between pages

--- pages/news/[newsid].js
+++ pages/news/[newsid].js
@@ -5,7 +5,7 @@ import { useRouter } from "next/router"
 function DetailPage() {
   const router = useRouter();
 
-  console.log (router.query.newsid);
+  const newsId = router.query.newsid;
 
     return <h1>The Detail Page</h1>
   }
