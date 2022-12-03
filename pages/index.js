8bbb6870236dbcbac726219a19bf245a824e00ed
unknown <lukacc008@gmail.com>
2022-12-03 13:59:32 +0100

    Tiny commit

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,15 @@ const DUMMY_MEETUPS = [
     {
         id:"m1",
         title: "First Meetup",
-        image: "https://upload.wikimedia.org/wikipedia/commons/d/db/The_confluence_of_the_Sava_into_the_Danube_at_Belgrade.jpg",
-        adress: "Belgrade Serbia Kalemegdan Fortress",
+        image: "https://media.cntraveler.com/photos/62ab85a86df361f5801fd45b/16:9/w_2580,c_limit/Tokyo_GettyImages-1319747119.jpg",
+        address: "Tokyo Japan",
         description: "This is a first meetup!"
     },
     {
         id:"m2",
         title: "Second Meetup",
-        image: "https://upload.wikimedia.org/wikipedia/commons/d/db/The_confluence_of_the_Sava_into_the_Danube_at_Belgrade.jpg",
-        adress: "Belgrade Serbia Kalemegdan Fortress",
+        image: "https://media.cntraveler.com/photos/62ab85a86df361f5801fd45b/16:9/w_2580,c_limit/Tokyo_GettyImages-1319747119.jpg",
+        address: "Tokyo Japan",
         description: "This is a second meetup!"
     }
 ]
