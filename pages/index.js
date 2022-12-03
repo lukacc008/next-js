641895eb591702c6f0f9907ff90d4f3f69ce9133
unknown <lukacc008@gmail.com>
2022-12-03 13:26:10 +0100

    Switched to my create-next-app and now i dont get INTERNAL SERVER ERROR

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
-// moj-domain.com/
+import MeetupList from "../components/meetups/MeetupList";
+
+
+const DUMMY_MEETUPS = [
+    {
+        id:"m1",
+        title: "First Meetup",
+        image: "https://upload.wikimedia.org/wikipedia/commons/d/db/The_confluence_of_the_Sava_into_the_Danube_at_Belgrade.jpg",
+        adress: "Belgrade Serbia Kalemegdan Fortress",
+        description: "This is a first meetup!"
+    },
+    {
+        id:"m2",
+        title: "Second Meetup",
+        image: "https://upload.wikimedia.org/wikipedia/commons/d/db/The_confluence_of_the_Sava_into_the_Danube_at_Belgrade.jpg",
+        adress: "Belgrade Serbia Kalemegdan Fortress",
+        description: "This is a second meetup!"
+    }
+]
 
 function HomePage() {
-  return <h1>The Home Page</h1>
-}
+  return <MeetupList meetups={DUMMY_MEETUPS} />
+};
 
-export default HomePage; 
+export default HomePage;
