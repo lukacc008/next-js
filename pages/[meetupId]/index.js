c9b6f97380538eda32487ae9c91aff5b1cfa53d5
unknown <lukacc008@gmail.com>
2022-12-04 13:25:44 +0100

    334. Adding custom components and CSS module

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://media.cntraveler.com/photos/62ab85a86df361f5801fd45b/16:9/w_2580,c_limit/Tokyo_GettyImages-1319747119.jpg"
+      title="First Meetup"
+      address="Tokyo Japan, Busan Street 8"
+      description="This is a first meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
