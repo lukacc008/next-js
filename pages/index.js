1b534628054b3ac2ac40cded6f0a6c03ac9da46f
unknown <lukacc008@gmail.com>
2022-12-05 15:16:37 +0100

    Simulated fetching data from a back-end, will change soon

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 
@@ -19,7 +20,13 @@ const DUMMY_MEETUPS = [
 ]
 
 function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />
+   const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+   useEffect(() => {
+   setLoadedMeetups(DUMMY_MEETUPS)
+   }, []);
+    
+  return <MeetupList meetups={loadedMeetups} />
 };
 
 export default HomePage;
