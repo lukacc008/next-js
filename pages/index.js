9df6cc89879b6551c2d97a8f10fca44ab8cdfa60
unknown <lukacc008@gmail.com>
2022-12-06 13:21:21 +0100

    336. getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,35 @@
-import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
-
 const DUMMY_MEETUPS = [
-    {
-        id:"m1",
-        title: "First Meetup",
-        image: "https://media.cntraveler.com/photos/62ab85a86df361f5801fd45b/16:9/w_2580,c_limit/Tokyo_GettyImages-1319747119.jpg",
-        address: "Tokyo Japan",
-        description: "This is a first meetup!"
+  {
+    id: "m1",
+    title: "First Meetup",
+    image:
+      "https://media.cntraveler.com/photos/62ab85a86df361f5801fd45b/16:9/w_2580,c_limit/Tokyo_GettyImages-1319747119.jpg",
+    address: "Tokyo Japan",
+    description: "This is a first meetup!",
+  },
+  {
+    id: "m2",
+    title: "Second Meetup",
+    image:
+      "https://media.cntraveler.com/photos/62ab85a86df361f5801fd45b/16:9/w_2580,c_limit/Tokyo_GettyImages-1319747119.jpg",
+    address: "Tokyo Japan",
+    description: "This is a second meetup!",
+  },
+];
+
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+
+export async function getStaticProps() {
+  // fetch data from an API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
     },
-    {
-        id:"m2",
-        title: "Second Meetup",
-        image: "https://media.cntraveler.com/photos/62ab85a86df361f5801fd45b/16:9/w_2580,c_limit/Tokyo_GettyImages-1319747119.jpg",
-        address: "Tokyo Japan",
-        description: "This is a second meetup!"
-    }
-]
-
-function HomePage() {
-   const [loadedMeetups, setLoadedMeetups] = useState([]);
-
-   useEffect(() => {
-   setLoadedMeetups(DUMMY_MEETUPS)
-   }, []);
-    
-  return <MeetupList meetups={loadedMeetups} />
-};
+  };
+}
 
 export default HomePage;
