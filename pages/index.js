69d13553a4ccc4e6c33aaa35b982636a74ad13f5
unknown <lukacc008@gmail.com>
2022-12-08 14:49:41 +0100

    338. SSR getServerSideProps alternative to getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -23,14 +23,28 @@ function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
   
-export async function getStaticProps() {
-  // fetch data from an API
+
+export async function getServerSideProps(context) {
+  const req = context.req;
+  const res = context.res;
+  
+  //fetch data from an API
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
-    },
-    revalidate: 1
+      meetups: DUMMY_MEETUPS
+    }
   };
 }
+
+// export async function getStaticProps() {
+//   // fetch data from an API
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//     revalidate: 1
+//   };
+// }
 
 export default HomePage;
