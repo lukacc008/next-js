e6b4a798b8314d2693c9231152bd5cd30c918710
unknown <lukacc008@gmail.com>
2022-12-11 16:59:52 +0100

    Connecting my code to MongoDB database.

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,11 +1,25 @@
+import { MongoClient } from 'mongodb';
+
 // URL ===  api/new-meetup   ---> It triggers a function that i define 
 // in this file.
 
-function handler(req, res) {
+async function handler(req, res) {
     if (req.method === "POST") {
         const data = req.body;
 
-        const { title, image, address, description } = data;
+                            //PASS CHANGED SO DONT TRY TO CONNECT TO MY CLUSTER
+      const client = await MongoClient.connect("mongodb+srv://lukacc008:123456789@cluster0.lept9un.mongodb.net/meetups?retryWrites=true&w=majority");
+      const db = client.db();
+
+      const meetupsCollection = db.collection("meetups");
+
+      const result = await meetupsCollection.insertOne({data});
+
+      console.log(result);
+
+      client.close();
+
+      res.status(201).json({ message: "Meetup inserted!" });
     }
 }
 
